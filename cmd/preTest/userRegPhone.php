6d50ed86a3adcd976876411174f08f58d480b745
Andrey Yartsev <a.yarcev@corp.nn.ru>
2015-04-13 07:52:02 +0300

    Auto-commit on 13.04.2015 08:52:02

--- cmd/preTest/userRegPhone.php
+++ cmd/preTest/userRegPhone.php
@@ -1,9 +1,9 @@
 <?php
 
 Dir::clear('C:/1/captures');
-SiteConfig::replaceConstant('more', 'TESTING', true);
-SiteConfig::updateSubVar('userReg', 'emailEnable', false);
-SiteConfig::updateSubVar('userReg', 'enable', true);
-SiteConfig::updateSubVar('userReg', 'phoneEnable', true);
-SiteConfig::updateSubVar('userReg', 'phoneConfirm', true);
+ProjectConfig::replaceConstant('more', 'TESTING', true);
+ProjectConfig::updateSubVar('userReg', 'emailEnable', false);
+ProjectConfig::updateSubVar('userReg', 'enable', true);
+ProjectConfig::updateSubVar('userReg', 'phoneEnable', true);
+ProjectConfig::updateSubVar('userReg', 'phoneConfirm', true);
 db()->query("TRUNCATE TABLE users");
